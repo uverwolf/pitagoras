48e255db221fc0f9e381039b9c55960e228336d6
Leoh <leonel.yam.meza@gmail.com>
2020-07-01 11:03:49 -0400

    Actualizacion de codigo para incluir metodo Scanner

--- src/pitagoras/Pitagoras.java
+++ src/pitagoras/Pitagoras.java
@@ -1,17 +1,26 @@
 package pitagoras;
-
+import java.util.Scanner;
 public class Pitagoras {
 	static void calculo(int a, int b) {
 		 a*=a;		
 		 b*=b;
 		int calculo = a+b;
-		System.out.println("La suma es : "+calculo);
+		
 		double raiz = Math.sqrt(calculo);
-		System.out.println("La raiz es: "+raiz);
+		System.out.println("Hipotenusa: "+raiz);
 	}
 
 	public static void main(String[] args) {
-		calculo(5,5);
+		Scanner num1 = new Scanner(System.in);
+		System.out.println("Ingrese primer valor:");
+		int valor1= num1.nextInt();
+		
+		Scanner num2 = new Scanner(System.in);
+		System.out.println("ingrese segundo valor:");
+		int valor2= num1.nextInt();
+		
+		
+		calculo(valor1,valor2);
 
 	}
 
